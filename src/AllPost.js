384474621a3daee983c8b0c1e170d56a92d58464
Isaac Contreras <iacontrerasg@icloud.com>
2020-03-30 18:23:42 -0600

    Add react-persist

--- src/AllPost.js
+++ src/AllPost.js
@@ -7,6 +7,7 @@ class AllPost extends Component {
     return (
       <div>
         <h1 className="post_heading">All Posts</h1>
+
         {this.props.posts.map(post => (
           <div key={post.id}>
             {post.editing ? (
@@ -23,7 +24,7 @@ class AllPost extends Component {
 
 const mapStateToProps = state => {
   return {
-    posts: state
+    posts: state.root
   };
 };
 export default connect(mapStateToProps)(AllPost);
--- src/index.js
+++ src/index.js
@@ -2,14 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { createStore } from "redux";
+import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
 
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import { PersistGate } from "redux-persist/integration/react";
+
 import postReducer from "./reducers/postReducer";
-const store = createStore(postReducer);
+
+const persistConfig = {
+  key: "root",
+  storage
+};
+
+const rootReducer = combineReducers({
+  root: postReducer
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+let store = createStore(persistedReducer);
+let persistor = persistStore(store);
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
